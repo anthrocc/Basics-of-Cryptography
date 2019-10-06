7974482e61ad26a792fb32fd525636e103f14599
anthrocc <anthony.roccasalva@gmail.com>
2019-10-06 19:04:10 -0400

    Finished activity B5

--- src/symmetric/ActivityB5.java
+++ src/symmetric/ActivityB5.java
@@ -14,22 +14,32 @@ import java.security.Key;
 public class ActivityB5 {
     public static void main(String[]args) throws Exception {
         String rawCT = readFile("data/MSGActivityB5.ct", StandardCharsets.US_ASCII);
-        System.out.println(" The raw CT is: " + rawCT);
-
-        byte[] ct = CryptoTools.hexToBytes(rawCT);
         byte[] key = "FACEBOOK".getBytes();
-        byte[] compKey = ~key;  // Get bitwise compliment of the key
-        // Decrypt with the bitwise compliment first and then with the regular key
+        byte[] compKey = complementByte(key);
+        byte[] ct = CryptoTools.hexToBytes(rawCT);
 
-        Key secret = new SecretKeySpec(key, "DES");
         Cipher cipher = Cipher.getInstance("DES/ECB/NoPadding");
+        Key secret = new SecretKeySpec(key, "DES");
+        Key compSecret = new SecretKeySpec(compKey, "DES");
+        cipher.init(Cipher.DECRYPT_MODE, compSecret);
+        byte[] ct1 = cipher.doFinal(ct);
+
         cipher.init(Cipher.DECRYPT_MODE, secret);
-        byte[] pt = cipher.doFinal(ct);
-        System.out.println("BK = " + new String(pt) + "<");
+        byte[] ct2 = cipher.doFinal(ct1);
 
-
+        System.out.println(new String(ct2));
     }
 
+    // Method to get the complement of a byte
+    public static byte[] complementByte(byte[] temp) {
+        byte[] ct = new byte[temp.length];
+        for (int i = 0; i < temp.length; i++) {
+            ct[i] = (byte) (~temp[i]);
+        }
+        return ct;
+    }
+
+    // Method to parse a file into a string
     static String readFile(String path, Charset encoding) throws IOException {
         byte[] encoded = Files.readAllBytes(Paths.get(path));
         return new String(encoded, encoding);
