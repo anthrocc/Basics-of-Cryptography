fe56844242d3be7fa65af596224ead21ef815cd7
anthrocc <anthony.roccasalva@gmail.com>
2019-10-06 19:15:52 -0400

    Finished B3

--- src/symmetric/ActivityB3.java
+++ src/symmetric/ActivityB3.java
@@ -12,21 +12,19 @@ public class ActivityB3
 
     public static void main(String[] args) throws Exception
     {
-        //byte[] pt = "EECS3481 Applied Cryptography".getBytes();
-        String knownCt = "4E51297B424F90D8B2ACD6ADF010DDC4"; // length = 32
-        String unKnownCt = "????????????????"; // length = 16
-        byte[] ct = CryptoTools.hexToBytes(knownCt + unKnownCt);
-        //byte[] ct = CryptoTools.hexToBytes("4E51297B424F90D8B2ACD6ADF010DDC4");
         byte[] key = "CSE@YORK".getBytes();
         byte[] iv = CryptoTools.hexToBytes("0123456701234567");
+        byte[] ct1 = CryptoTools.hexToBytes("4E51297B424F90D8");
+        byte[] ct2 = CryptoTools.hexToBytes("B2ACD6ADF010DDC4");  // Idk how he got this
 
         Key secret = new SecretKeySpec(key, "DES");
         Cipher cipher = Cipher.getInstance("DES/CBC/PKCS5Padding");
-        AlgorithmParameterSpec aps = new IvParameterSpec(iv);
-        cipher.init(Cipher.DECRYPT_MODE, secret, aps);
-        byte[] pt = cipher.doFinal(ct);
 
-        System.out.println("PT = " + new String(pt) + "<");
+        IvParameterSpec ips = new IvParameterSpec(ct1);
+        cipher.init(Cipher.DECRYPT_MODE, secret, ips);
+        byte[] pt = cipher.doFinal(ct2);
+
+        System.out.println("PT = " + new String(pt));
 
 
     }
